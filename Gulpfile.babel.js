1a1d7fff5075e3b0c70030d795b0ae9b1b0a58c6
Rodrigo Souza <ro_web1@hotmail.com>
2016-10-06 10:23:22 -0300

    upload workflow

---
+++ Gulpfile.babel.js
@@ -0,0 +1,62 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import rupture from 'rupture';
+import cssnano from 'cssnano';
+import gcmq from 'gulp-group-css-media-queries';
+import imagemin from 'gulp-imagemin';
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import stylus from 'gulp-stylus';
+import uglify from 'gulp-uglify';
+import lost from 'lost';
+import poststylus from 'poststylus';
+import autoprefixer from 'autoprefixer';
+
+const dirs = {
+	src:'src',
+	dest:'build'
+}
+
+gulp.task('styles', ()=> {
+	gulp.src('./src/stylus/main.styl')
+	.pipe(plumber())
+  .pipe(stylus({
+    use:[
+      poststylus([ autoprefixer({ browsers: ["last 3 version", "> 1%", "ie 8"] })]),
+      rupture()
+    ]
+  }))
+  .pipe(postcss([
+  	lost(),
+  	cssnano()
+  ]))
+  .pipe(gcmq())
+  .pipe(gulp.dest('./build/css/'))
+});
+
+
+gulp.task('scripts', ()=> {
+  return gulp.src('./src/js/main.js')
+  // .pipe(uglify())
+  .pipe(gulp.dest('./build/js'))
+});
+
+gulp.task('imgoptimizer', ()=> {
+  return gulp.src('./src/img/*.{jpg,gif,png,svg}')
+  .pipe(plumber())
+  .pipe(imagemin({
+    optmizationLevel: 5,
+    progressive: true,
+    interlaced: true
+  }))
+  .pipe(gulp.dest('./build/images/'))
+});
+
+gulp.task('watch', ()=> {
+  gulp.watch('./src/stylus/*.styl', ['styles']);
+  gulp.watch('./src/js/*.js', ['scripts']);
+  gulp.watch('./src/img/*.{jpg,gif,png,svg}', ['imgoptimizer']);
+});
+
+
+gulp.task('default', ['watch','imgoptimizer']);
---
+++ Readme.md
@@ -0,0 +1,33 @@
+## Intro
+
+Este é o workflow inicial para facilitar o desenvolvimento de lojas para a zFast
+
+## Instalação
+
+Para instalar as dependências do package.json acesse a pasta correspondente via cmd e execute o seguinte comando: 
+
+```
+npm i
+```
+
+## Rodando o workflow
+
+Para rodar o workflow basta executar o seguinte comando em seu cmd: 
+
+```
+gulp
+```
+
+## Observações
+
+Todos os caminhos de **DESTINO**(.dest) contidos no arquivo **Gulpfile.babel** são fictícios e devem ser alterados pelo caminho da loja em que está trabalhando, por exemplo, onde está:
+
+```
+./build/images/
+```
+- Substituir pelo caminho de sua loja. Exemplo: 
+
+```
+//192.168.0.2/00010538/images/
+```
+- **Obs**: Existem mais caminhos de destino no arquivo Gulpfile.babel para seream alterados para o que está utilizando
